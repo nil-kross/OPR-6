4c4e77d860c11fdce6a696f682be23553dc5ef63
Nikita Klushnikov <nikita.klushnikov@itechart-group.com>
2018-12-12 10:27:39 +0300

    Revert "Add GraphForm class."
    This reverts commit db745258f0f1ad0c0025fee779bd07871e7328ae.

--- Forms/GraphForm.cs
+++ Forms/GraphForm.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,92 +12,9 @@ namespace Lomtseu
 {
     public partial class GraphForm : Form
     {
-        private Double[][] strategiesArray;
-
-        public GraphForm(Double[][] strategies = null)
+        public GraphForm()
         {
             InitializeComponent();
-
-            this.strategiesArray = strategies ?? new double[2][];
-            this.strategiesArray[0] = new double[7] { -6, -1, 1, 4, 7, 4, 3 };
-            this.strategiesArray[1] = new double[7] { 7, -2, 6, 3, -2, -5, 7 };
-
-            this.ResizeLayout();
-        }
-
-        public void Draw()
-        {
-            this.DrawStrategies();
-        }
-
-        protected void ResizeLayout()
-        {
-            this.pictureBox.Location = new Point(0, 0);
-            this.pictureBox.Size = new Size(this.ClientSize.Width, this.ClientSize.Height);
-
-            this.DrawStrategies();
-        }
-
-        private void DrawStrategies()
-        {
-            Graphics graphics = this.pictureBox.CreateGraphics();
-            Double maxValue = 0;
-            Double minValue = 0;
-
-            graphics.SmoothingMode = SmoothingMode.AntiAlias;
-            graphics.Clear(Color.White);
-            for (var r = 0; r < this.strategiesArray.Length; r++)
-            {
-                maxValue = Math.Max(maxValue, this.strategiesArray[r].Max());
-                minValue = Math.Min(minValue, this.strategiesArray[r].Min());
-            }
-
-            {
-                Double gapValue = 20.0 / 100;
-                var deltaValue = Math.Abs(maxValue - minValue);
-                var hieghtValue = this.ClientSize.Height / deltaValue;
-
-                // Соединительные линии
-                {
-                    Pen pen = new Pen(Color.Black, 1);
-
-                    for (var r = 0; r < this.strategiesArray[0].Length; r++)
-                    {
-                        var leftValue = this.ClientSize.Height - (this.strategiesArray[0][r] - minValue) * hieghtValue;
-                        var rightValue = this.ClientSize.Height - (this.strategiesArray[1][r] - minValue) * hieghtValue;
-
-                        graphics.DrawLine(pen, (Int32)(this.ClientSize.Width * (0 + gapValue)), (Int32)leftValue, (Int32)(this.ClientSize.Width * (1 - gapValue)), (Int32)rightValue);
-                    }
-                }
-                // Вертикальные линии
-                {
-                    Pen pen = new Pen(Color.Black, 3);
-
-                    graphics.DrawLine(pen, (Int32)(this.ClientSize.Width * (0 + gapValue)), 0, (Int32)(this.ClientSize.Width * (0 + gapValue)), this.ClientSize.Height);
-                    graphics.DrawLine(pen, (Int32)(this.ClientSize.Width * (1 - gapValue)), 0, (Int32)(this.ClientSize.Width * (1 - gapValue)), this.ClientSize.Height);
-                }
-                // Горизонтальные штрихи
-                {
-                    Pen pen = new Pen(Color.Black, 1);
-
-                    for (var i = 0; i < deltaValue; i++)
-                    {
-                        var currHeightValue = i * hieghtValue;
-
-                        graphics.DrawLine(pen, (Int32)(this.ClientSize.Width * (1 - gapValue)), (Int32)currHeightValue, (Int32)(this.ClientSize.Width * (1 - gapValue)) + 5, (Int32)currHeightValue);
-                        graphics.DrawLine(pen, (Int32)(this.ClientSize.Width * (0 + gapValue)), (Int32)currHeightValue, (Int32)(this.ClientSize.Width * (0 + gapValue)) - 5, (Int32)currHeightValue);
-                    }
-                }
-            }
-        }
-
-        private void OnGraphFormResize(object sender, EventArgs e)
-        {
-            this.ResizeLayout();
-        }
-
-        private void OnPictureBoxClick(Object sender, EventArgs e) {
-            this.ResizeLayout();
         }
     }
 }
